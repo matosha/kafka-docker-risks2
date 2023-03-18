c0a448f4d4735c08f5003d797c3b7b4eed5cce5b
Your Name <rh3tro@gmail.com>
2023-03-18 17:28:06 +0100

    commit to master

---
+++ get.py
@@ -0,0 +1,13 @@
+import requests,json
+from kafka import KafkaConsumer
+
+consumer = KafkaConsumer('tenable', group_id='my-group', bootstrap_servers=['localhost:9092'])
+
+print("getty")
+for message in consumer:
+    consumer.commit() 
+    # message value and key are raw bytes -- decode if necessary!
+    # e.g., for unicode: `message.value.decode('utf-8')`
+    print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                                          message.offset, message.key,
+                                          message.value))
---
+++ push.py
@@ -0,0 +1,10 @@
+import requests,json
+from kafka import KafkaProducer
+
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
+
+head=["Accept application/json"]
+response=requests.get("https://api.msrc.microsoft.com/cvrf/v2.0/updates",  headers={"Accept":"application/json"})
+#print(response.json())
+for i in range(0,10):
+        producer.send('tenable', json.dumps(response.json()).encode('utf-8'))
